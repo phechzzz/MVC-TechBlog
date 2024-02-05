25d98df859b170a1f9be225dcce0123787b7ad0f
phechz <peytonengborg@gmail.com>
2024-02-05 12:26:01 -0700

    styling, handlebars,

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
-
-
 require('dotenv').config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  // For JawsDB on Heroku
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  // For local development
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
